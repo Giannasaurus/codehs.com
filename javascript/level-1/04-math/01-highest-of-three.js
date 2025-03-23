00563a3dc6d0b8f33b79a9888cbef54a03961ade
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-24 03:16:40 +0800

    Create 01-highest-of-three.js

---
+++ javascript/level-1/04-math/01-highest-of-three.js
@@ -0,0 +1,13 @@
+/** Write a function that takes in three numbers and returns the highest number. */
+
+function highestNumber(num1, num2, num3){
+    return Math.max(num1, num2, num3)
+}
+
+/** passed test cases
+  * test case 1:
+    > highestNumber(1, 7, 10)
+  * test case 2:
+    > highestNumber(25, 4, -5)
+  * test case 3:
+    > highestNumber(-1, -20, -2) */
