5a85b4bfcbc57436da5fb311569ca56df583506c
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-23 14:41:28 +0800

    Create 03-multiple-arguments.js

---
+++ javascript/level-1/functions/03-multiple-arguments.js
@@ -0,0 +1,17 @@
+/** Write a function that receives two arguments, a and b. Return the second argument. */
+
+function twoArgs(a, b) {
+    return b
+}
+
+/** passed test cases
+  * test case 1:
+    > twoArgs('mac', 'cheese')
+  * test case 2:
+    > twoArgs('surf', 'turf')
+  * test case 3:
+    > twoArgs('apples', 'oranges')
+  * test case 4:
+    > twoArgs('tracy', 'karel')
+  * test case 5:
+    > twoArgs(5, 9) */
