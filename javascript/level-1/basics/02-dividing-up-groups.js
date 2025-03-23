6652e405ceab311ac4cb81ef1be7be5cb4216a8c
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-23 14:06:59 +0800

    Update 02-dividing-up-groups.js

--- javascript/level-1/basics/02-dividing-up-groups.js
+++ javascript/level-1/basics/02-dividing-up-groups.js
@@ -1,4 +1,6 @@
-/** Write a function that takes in a number of people and a number of groups. The people will be split up into the groups evenly (there might be left over people). Return the number of people in each group.
+/** Write a function that takes in a number of people and a number of groups.
+    The people will be split up into the groups evenly (there might be left over people).
+    Return the number of people in each group.
     Hint: Use Math.floor() to make sure the result is an integer. */
 
 function splitGroups(people, groups) {
