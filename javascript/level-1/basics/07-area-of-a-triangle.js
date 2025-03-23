c7c655af7d6d0ccc23057074a88f80985a9e0f3c
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-23 14:33:30 +0800

    Create 07-area-of-a-triangle.js

---
+++ javascript/level-1/basics/07-area-of-a-triangle.js
@@ -0,0 +1,16 @@
+/** Write a function that takes two numbers that represent the base and the height of a triangle and returns the area of the triangle. */
+
+function triangleArea(base, height) {
+	var area = base*height/2
+	return area
+}
+
+/** passed test cases
+  * test case 1:
+    > triangleArea(2, 5)
+  * test case 2:
+    > triangleArea(10, 8)
+  * test case 3:
+    > triangleArea(5, 9)
+  * test case 4:
+    > triangleArea(26, 14)  */
