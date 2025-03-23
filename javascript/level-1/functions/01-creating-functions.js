05ccab0405807b825418de6fcc99245c01e3f492
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-24 03:08:15 +0800

    Delete javascript directory

--- javascript/level-1/functions/01-creating-functions.js
+++
@@ -1,9 +0,0 @@
-/** Write a function named sayHello that does not take in any parameters and returns the string, “Hello, world!” */
-
-function sayHello() {
-    return "Hello, world!"
-}
-
-/** passed test cases
-  * test case 1:
-    > sayHello() */
