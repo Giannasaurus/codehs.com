e3e89de2c74207b39c6c8f8ef6eb0b18adf442a5
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-24 22:53:34 +0800

    Create 02-first.js

---
+++ javascript/level-1/05-arrays/02-first.js
@@ -0,0 +1,15 @@
+/** Given an array containing any kind of elements, return the first element. */
+
+function first(array) {
+    return array[0]
+}
+
+/** passed test cases
+  * test case 1:
+    > first([1, 2, 3])
+  * test case 2:
+    > first(['first', 'second', 'last'])
+  * test case 3:
+    > (hidden)
+  * test case 4:
+    > (hidden) */
