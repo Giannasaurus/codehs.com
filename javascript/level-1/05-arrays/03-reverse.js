3f1bb1df5f5e286df7ffe0d2526840d845181003
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-24 23:07:21 +0800

    Create 03-reverse.js

---
+++ javascript/level-1/05-arrays/03-reverse.js
@@ -0,0 +1,22 @@
+/** Write this function reverse that will return an array in reversed order.
+
+  * For example:
+    > reverse([1, 2, 3]) => [3, 2, 1] */
+
+function reverse(array) {
+    return array.reverse()
+}
+
+/** passed test cases
+  * test case 1:
+    > reverse([1,2,3])
+  * test case 2:  
+    > reverse(['breakfast', 'lunch', 'dinner'])
+  * test case 3:
+    > reverse(['cart', 'horse'])
+  * test case 4:
+    > reverse(['Is', 'it', 'crazy', 'how', 'writing', 'lists', 'backwards', 'creates', 'backwards', 'lists', 'writing', 'how', 'crazy', 'it', 'is'])
+  * test case 5:
+    > (hidden)
+  * test case 6:
+    > (hidden) */
