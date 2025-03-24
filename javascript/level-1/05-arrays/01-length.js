44c6c529f98a4ec8dc526fd4428bec887382dbed
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-24 21:33:18 +0800

    Create 01-length.js

---
+++ javascript/level-1/05-arrays/01-length.js
@@ -0,0 +1,15 @@
+/** Given an array, return the length of that array. */
+
+function length(array) {
+    return array.length
+}
+
+/** passed test cases
+  * test case 1:
+    > length([1, 2, 3])
+  * test case 2:
+    > length([])
+  * test case 3:
+    > (hidden case)
+  * test case 4:
+    > (hidden case) */
