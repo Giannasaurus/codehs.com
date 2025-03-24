ea8f5a9509522ab958525a095acb6f83136ec8b3
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-24 20:24:05 +0800

    Create 05-is-the-number-even.js

---
+++ javascript/level-1/04-math/05-is-the-number-even.js
@@ -0,0 +1,16 @@
+/** Write a function that takes in an integer and checks if the number is even.
+    The function should return either true or false. */
+
+function isEvenNumber(num) {
+    return num%2===0 ? true : false
+}
+
+/** passed test cases
+  * test case 1:
+    > isEvenNumber(2)
+  * test case 2:
+    > isEvenNumber(25)
+  * test case 3:
+    > isEvenNumber(-3)
+  * test case 4:
+    > isEvenNumber(0) */
