b15f3e4a2e45acc732ff7ae19b449ab156516eac
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-24 09:41:10 +0800

    Create 04-perimeter-of-a-rectangle.js

---
+++ javascript/level-1/04-math/04-perimeter-of-a-rectangle.js
@@ -0,0 +1,15 @@
+/** Write a function that takes in two numbers that represent the height and the width of of a rectangle and returns the perimeter. */
+
+function perimeter(height,width){
+    return (height*2)+(width*2)
+}
+
+/** passed test cases
+  * test case 1:
+    > perimeter(25, 5)
+  * test case 2:
+    > perimeter(30, 4)
+  * test case 3:
+    > perimeter(100, 9)
+  * test case 4:
+    > perimeter(12, 4) */
