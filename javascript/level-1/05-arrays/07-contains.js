6bc6bfcce3a215febb7f58db51467e04b7973be6
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-25 21:46:19 +0800

    Create 07-contains.js

---
+++ javascript/level-1/05-arrays/07-contains.js
@@ -0,0 +1,20 @@
+/** Write the function contains that will return true if an array contains the element el.
+
+  * For example:
+    > contains(['glorp', 'glop', 'gloop'], 'gloop') => true */
+
+function contains(array, el) {
+    return array.includes(el)
+}
+
+/** passed test cases
+  * test case 1:
+    > contains([1, 2, 3], 2)
+  * test case 2:
+    > contains(['squirtle', 'charmander', 'bulbasaur'], 'pikachu')
+  * test case 3:
+    > contains(['t', 'e', 'a', 'm'], 'i')
+  * test case 4:
+    > (hidden)
+  * test case 5:
+    > (hidden) */
