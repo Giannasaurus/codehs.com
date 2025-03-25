adb04fceeabe4fdc5d1947f5ea5a9b2834b2edee
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-25 22:17:38 +0800

    Create 08-last.js

---
+++ javascript/level-1/05-arrays/08-last.js
@@ -0,0 +1,20 @@
+/** Write the function last that will return the last element of array.
+    You can assume that array has at least one element. */
+
+function last(array) {
+    return array[array.length-1]
+}
+
+/** passed test cases
+  * test case 1:
+    > last(['monday', 'tuesday', 'wednesday', 'thursday', 'friday'])
+  * test case 2:
+    > last(['a new hope', 'the empire strikes back', 'return of the jedi'])
+  * test case 3:
+    > last(['chance', 'chance', 'chance'])
+  * test case 4:
+    > (hidden)
+  * test case 5:
+    > (hidden)
+  * test case 6:
+    > (hidden) */
