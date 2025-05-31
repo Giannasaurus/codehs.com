15dfaf6496c82ad9e006ea12b64c57f8c378008e
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-01 00:07:32 +0800

    Create 08-favorite-song.js

---
+++ javascript/level-1/02-functions/08-favorite-song.js
@@ -0,0 +1,12 @@
+/** Write a function favoriteSong that takes one parameter: songName (a string).
+    Return the string 'Now playing: ' followed by the songName. */
+
+function favoriteSong(songName) {
+    return `Now playing: ${songName}`;
+}
+
+/** passed test cases
+  * favoriteSong('Blinding Lights')
+  * favoriteSong('Bad Habit')
+  * favoriteSong('Good 4 U')
+  * favoriteSong('drivers license') */
