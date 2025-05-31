22618cc2a158671371fbf4633894b138155ad1f5
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-01 00:05:59 +0800

    Create 07-double-the-number.js

---
+++ javascript/level-1/02-functions/07-double-the-number.js
@@ -0,0 +1,11 @@
+/** Write a function called doubleNumber that takes one parameter: num (a number).
+  The function should return the number multiplied by 2. */
+
+function doubleNumber(num) {
+    return num*2;
+}
+
+/** passed test cases
+  * doubleNumber(5)
+  * doubleNumber(10)
+  * doubleNumber(0) */
