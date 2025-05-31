ddfc1fbe13be35e71245ec181940ec3757233656
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-05-31 21:25:13 +0800

    Create 11-social-media-follower-boost.js

---
+++ javascript/level-1/01-basics/11-social-media-follower-boost.js
@@ -0,0 +1,14 @@
+/** Write a function followerGoal that takes two parameters: currentFollowers (an integer) and newFollowers (an integer).
+    Return the total number of followers after gaining double the newFollowers amount. */
+
+function followerGoal(currentFollowers, newFollowers) {
+    let totalFollowers = currentFollowers + newFollowers*2;
+    
+   return totalFollowers;
+} 
+
+/** passed test cases
+  * followerGoal(500, 50)
+  * followerGoal(1000, 100)
+  * followerGoal(300, 0)
+  * followerGoal(750, 25) */
