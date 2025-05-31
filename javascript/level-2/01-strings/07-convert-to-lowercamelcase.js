92b10c7cc5e68d29aa3d879d81bd8e8d4346116c
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-01 00:00:37 +0800

    Create 07-convert-to-lowercamelcase.js

---
+++ javascript/level-2/01-strings/07-convert-to-lowercamelcase.js
@@ -0,0 +1,19 @@
+/** Write a function in JavaScript that takes in a string and converts it from snake_case to lowerCamelCase
+    You should remove all underscores and just return the given value but in lower camel case where the first word starts with a lower case letter each following word starts with an uppercase letter. */
+
+function snakeToLowerCamelCase(value) {
+    const words = value.split('_');
+    const camelCased = words[0].toLowerCase() + 
+        words.slice(1).map(word =>
+            word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        ).join('');
+    return caamelCased;
+}
+
+/** passed test cases
+  * snakeToLowerCamelCase('very_verbose_function')
+  * snakeToLowerCamelCase('return_result')
+  * snakeToLowerCamelCase('simple')
+  * snakeToLowerCamelCase('Starting_caps')
+  * snakeToLowerCamelCase('test')
+  * snakeToLowerCamelCase('get_thing') */
