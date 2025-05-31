7ae4d8212ae9868d246f9a446ea7ff2eb1dcf5f2
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-01 00:28:37 +0800

    Create 10-treasure-room-access.js

---
+++ javascript/level-1/02-functions/10-treasure-room-access.js
@@ -0,0 +1,8 @@
+/** Write a function canEnterVault that takes a boolean hasKey and returns ‘Access granted’ if true, and ‘Access denied’ if false. */
+
+function canEnterVault(hasKey) {
+	return hasKey ? "Access granted" : "Access denied";
+}
+
+/** passed test cases
+  * */
