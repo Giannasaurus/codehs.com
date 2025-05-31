0346b65b47b9b383c9cafdab76520f0c1227f9a8
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-01 00:02:43 +0800

    Create 05-create-a-greeting.js

---
+++ javascript/level-1/02-functions/05-create-a-greeting.js
@@ -0,0 +1,11 @@
+/** Write a function called createGreeting that takes one parameter: name (a string).
+    The function should return a greeting in the format: “Hello, [name]!” */
+
+function createGreeting(name) {
+    return `Hello, ${name}!`;
+}
+
+/** passed test cases
+  * createGreeting("Alice")
+  * createGreeting("Bob")
+  * createGreeting("Charlie") */
