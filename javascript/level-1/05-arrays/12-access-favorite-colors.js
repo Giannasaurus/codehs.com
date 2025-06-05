8e8fe4bb05701083c21abfabfc2c1dc22ec08918
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-05 17:20:22 +0800

    Create 12-access-favorite-colors.js

---
+++ javascript/level-1/05-arrays/12-access-favorite-colors.js
@@ -0,0 +1,11 @@
+/** Write a function favoriteColors that takes one parameter: colors (an array of three strings).
+    The function should return a string in the format: “My favorite colors are [color1], [color2], and [color3].” */
+
+function favoriteColors(colors) {
+    return `My favorite colors are ${colors[0]}, ${colors[1]}, and ${colors[2]}.`
+}
+
+/** passed test cases
+  * favoriteColors(["blue", "green", "red"])
+  * favoriteColors(["yellow", "purple", "pink"])
+  * favoriteColors(["black", "white", "gray"]) */
