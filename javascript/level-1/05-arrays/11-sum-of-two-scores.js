f60bd1e5e68b56c62ced4758026d341cc3bf9f03
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-05 17:16:34 +0800

    Create 11-sum-of-two-scores.js

---
+++ javascript/level-1/05-arrays/11-sum-of-two-scores.js
@@ -0,0 +1,11 @@
+/** Write a function sumFirstTwo that takes one parameter: scores (an array containing at least two numbers).
+    The function should return the sum of the first two elements in the array. */
+
+function sumFirstTwo(scores) {
+  return scores[0] + scores[1];
+}
+
+/** passed test cases
+  * sumFirstTwo([10, 20, 30])
+  * sumFirstTwo([5, 15])
+  * sumFirstTwo([100, 200, 300, 400]) */
