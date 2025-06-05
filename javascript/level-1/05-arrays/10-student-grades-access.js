4f489ea84dbce34b3925d0bbb96ef6c092388d91
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-05 17:13:30 +0800

    Create 10-student-grades-access.js

---
+++ javascript/level-1/05-arrays/10-student-grades-access.js
@@ -0,0 +1,11 @@
+/** Write a function getStudentGrade that takes two parameters: grades (an array of numbers) and index (a number).
+    The function should return the grade at the specified index in the array. */
+
+function getStudentGrade(grades, index) {
+    return grades[index];
+}
+
+/** passed test cases
+  * getStudentGrade([85, 92, 78, 90], 2)
+  * getStudentGrade([70, 88, 94], 0)
+  * getStudentGrade([100, 99, 98, 97], 3) */
