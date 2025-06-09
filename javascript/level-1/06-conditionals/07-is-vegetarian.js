e89dd63c2e8ef2c48919b32f037a96804b8f03ab
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-09 16:14:38 +0800

    Create 07-is-vegetarian.js

---
+++ javascript/level-1/06-conditionals/07-is-vegetarian.js
@@ -0,0 +1,14 @@
+/** Create a function isVegetarian that takes one parameter: a string dishName.
+    The function should return:
+    - true if the dish is “salad”, “pasta”, or “tofu stir fry”
+    - false for any other dish */
+
+function isVegetarian(dishName) {
+	return dishName === "salad" ||
+	       dishName === "pasta" ||
+	       dishName === "tofu stir fry";
+}
+
+/** passed test cases
+  *
+*/
