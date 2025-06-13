816970dba9334ae1f6bfce59a65c8ab89e60c9ea
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-13 14:59:48 +0800

    Create 02-jump-counter.js

---
+++ javascript/level-1/07-iteration/02-jump-counter.js
@@ -0,0 +1,29 @@
+/**
+ * You’re practicing long jumps in gym class.
+ * Each jump adds 3 meters to your total distance.
+ * Write a function countJumpsNeeded that determines how many jumps it takes to reach or exceed a certain distance.
+ * The function should take one parameter:
+   - distance (integer)
+ * It should return the number of jumps needed to reach or exceed that distance.
+ */
+
+function countJumpsNeeded(distance) {
+    let jumps = 0;
+    let total = 0;
+    
+    while (total < distance) {
+        jumps++;
+        total += 3;
+    }
+    
+    return jumps;
+}
+
+/** 
+ * passed test cases
+ * countJumpsNeeded(10)
+ * countJumpsNeeded(1)
+ * countJumpsNeeded(15)
+ * countJumpsNeeded(9)
+ * countJumpsNeeded(0)
+ */
